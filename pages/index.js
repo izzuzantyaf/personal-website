906c83f614a36bf17a8d7d08db764fed70ee2799
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2020-10-18 11:03:02 +0700

    litle update

--- pages/index.js
+++ pages/index.js
@@ -12,20 +12,13 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">My Personal Website</a>
+          Hi there, <a href="https://nextjs.org">I'm Izzu</a>
         </h1>
+        <p className="description">
+          My site is under development
+        </p>
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
     </div>
   )
 }
