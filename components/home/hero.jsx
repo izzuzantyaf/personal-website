961408bf6ac97c6df6c4492dc7a0814a1e3d3b62
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2020-12-22 17:23:11 +0700

    little change on code structure and look

--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -1,12 +1,31 @@
 export default function Hero() {
     return (
-        <div className="hero">
-            <div className="container mx-auto">
-                <h1 className="
-                        text-5xl font-black
-                        text-center text-gray-700">Hi there, <span className="text-blue-700">I'm Izzu</span></h1>
+        <div className="hero p-4 bg-gray-300">
+            <div className="container flex flex-col mx-auto h-screen justify-center gap-10">
 
-                <h3 className="text-center text-gray-700 font-semibold">Bio is not available yet</h3>
+                <div className="greeting">
+                    <h1 className="
+                        text-5xl font-black
+                        text-center">Hi there, <span className="text-secondary">I'm Izzu</span>
+                    </h1>
+
+                    <h3 className="text-center font-semibold">Netflix, Code, and Travel</h3>
+                </div>
+
+                {/* <div className="recent-projects bg-indigo-100 flex flex-col items-center gap-4 rounded-2xl">
+                    <h2 className="text-2xl font-bold">Recent projects</h2>
+                    <div className="project-cards flex gap-4 w-full">
+                        <div className="project-card flex-grow bg-indigo-300 h-20">
+                            content
+                        </div>
+                        <div className="project-card flex-grow bg-indigo-300 h-20">
+                            content
+                        </div>
+                        <div className="project-card flex-grow bg-indigo-300 h-20">
+                            content
+                        </div>
+                    </div>
+                </div> */}
             </div>
         </div>
     )
