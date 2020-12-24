24602390fb40f9916e73dac13a60f49196a16687
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2020-12-24 20:55:04 +0700

    move social media list to footer

--- components/footer/footer.jsx
+++ components/footer/footer.jsx
@@ -1,8 +1,29 @@
+import socialMedia from '../../constants/social-media'
+
 export default function Footer() {
+
+  const SocialMediaList = () => {
+
+    const SocialMediaBtn = (props) => <a href={props.socmed.link} target="_blank" rel="noopener noreferrer" className={`px-2 py-1 rounded-md transition duration-500 ease-in-out bg-gray-800 font-semibold transform hover:${props.socmed.bgHover} text-gray-500 hover:${props.socmed.colorHover} hover:-translate-y-1 hover:scale-110`}>
+      {props.socmed.name}
+    </a>
+
+    return (
+      <div className="social-media md:justify-self-end flex justify-center gap-4">
+        {
+          socialMedia.map((socmed, index) =>
+            <SocialMediaBtn socmed={socmed} key={index} />
+          )
+        }
+      </div>
+    )
+  }
+
   return (
-    <footer className="footer p-4 bg-gray-900">
-      <div className="container mx-auto">
-        <h4 className="font-bold text-xl text-white text-center ">izzuzantyaf.</h4>
+    <footer className="footer p-8 bg-gray-900">
+      <div className="container grid grid-cols-1 md:grid-cols-2 gap-4 mx-auto">
+        <h4 className="font-bold text-xl text-white text-center md:text-left">izzuzantyaf.</h4>
+        <SocialMediaList />
       </div>
     </footer>
   )
--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -1,38 +1,17 @@
-import socialMedia from '../../constants/social-media'
-
 export default function Hero() {
 
-    const Greeting = () => (
+    const Greeting = () =>
         <div className="greeting">
             <h1 className="text-4xl sm:text-5xl md:text-6xl font-black text-center">Hi there, <span className="text-blue-600">I'm Izzu</span>
             </h1>
 
-            <h3 className="text-center sm:text-2xl font-semibold">Netflix, Code, and Travel</h3>
+            <h3 className="text-center sm:text-2xl font-semibold">a Software Developer</h3>
         </div>
-    )
-
-    const SocialMediaList = () => {
-
-        const SocialMediaBtn = (props) => <a key={props.index} href={props.socmed.link} target="_blank" rel="noopener noreferrer" className={`px-2 py-1 rounded-md transition duration-500 ease-in-out bg-gray-400 font-semibold transform hover:${props.socmed.bgHover} hover:${props.socmed.colorHover} hover:-translate-y-1 hover:scale-110`}>
-            {props.socmed.name}
-        </a>
-
-        return (
-            <div className="social-media flex justify-center gap-4">
-                {
-                    socialMedia.map((socmed, index) =>
-                        <SocialMediaBtn socmed={socmed} index={index} />
-                    )
-                }
-            </div>
-        )
-    }
 
     return (
         <div className="hero px-4 bg-gray-200">
             <div className="container flex flex-col mx-auto h-screen justify-center gap-10">
                 <Greeting />
-                <SocialMediaList />
             </div>
         </div>
     )
