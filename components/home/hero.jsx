b3882b162457606914562fd79d80bc5377e970a6
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2020-12-30 13:41:28 +0700

    add dark mode feature

--- components/home/hero.jsx
+++ components/home/hero.jsx
@@ -2,14 +2,14 @@ export default function Hero() {
 
     const Greeting = () =>
         <div className="greeting">
-            <h1 className="text-4xl sm:text-5xl md:text-6xl font-black text-center">Hi there, <span className="text-blue-600">I'm Izzu</span>
+            <h1 className="text-4xl sm:text-5xl md:text-6xl font-black text-center dark:text-gray-300">Hi there, <span className="text-blue-600">I'm Izzu</span>
             </h1>
 
-            <h3 className="text-center sm:text-2xl font-semibold">a Digital Craftman</h3>
+            <h3 className="text-center sm:text-2xl font-semibold  dark:text-gray-300">a Digital Craftman</h3>
         </div>
 
     return (
-        <div className="hero px-4 bg-gray-200">
+        <div className="hero px-4 bg-gray-200 dark:bg-gray-900">
             <div className="container flex flex-col mx-auto h-screen justify-center gap-10">
                 <Greeting />
             </div>
--- components/home/projects.jsx
+++ components/home/projects.jsx
@@ -1,13 +1,13 @@
 export default function Projects() {
   return (
-    <div className="my-projects bg-gray-300 p-8">
+    <div className="my-projects bg-gray-300 dark:bg-gray-800 p-8">
 
       <div className="container mx-auto">
-        <h3 className="title text-center text-4xl sm:text-5xl font-black">My works</h3>
+        <h3 className="title text-center text-4xl sm:text-5xl font-black dark:text-gray-300">My works</h3>
 
         <div className="project-list mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8 justify-center">
 
-          <div className="project-card md:col-span-2 lg:col-start-2 lg:col-span-2 p-4 bg-white shadow-lg rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+          <div className="project-card md:col-span-2 lg:col-start-2 lg:col-span-2 p-4 bg-white dark:bg-gray-900 dark:text-gray-300 shadow-lg rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
             <img src="/images/projects/labfisdas.png" alt="Tel-U physics lab web project" className="rounded-xl" />
             <div className="project-card-footer flex flex-col gap-4 mt-4">
               <h3 className="project-title font-bold text-xl md:text-2xl">Tel-U Physics Laboratory Website</h3>
