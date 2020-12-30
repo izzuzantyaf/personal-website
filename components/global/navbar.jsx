c142168c723a5b4a372e7fb3fd4e35e4c8c9999b
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2020-12-30 11:35:53 +0700

    fix dark mode logic

--- components/global/navbar.jsx
+++ components/global/navbar.jsx
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react'
 
 const Brand = () =>
-  <div className="brand font-black text-3xl">
+  <div className="brand font-black text-3xl dark:text-gray-500">
     IZZ.
   </div>
 
 const DarkModeSlider = () => {
+
   const [isDarkMode, setisDarkMode] = useState(false)
 
   function controlMode() {
-    if (!isDarkMode) {
+    if (!isDarkMode || window.matchMedia('(prefers-color-scheme: light)').matches) {
       localStorage.theme = 'dark'
       setisDarkMode(true)
       document.querySelector('html').classList.add('dark')
@@ -22,17 +23,17 @@ const DarkModeSlider = () => {
 
   useEffect(() => {
     if (localStorage.theme === undefined) localStorage.theme = 'light'
-    const slider = document.querySelector('.dark-mode-slider')
-    slider.addEventListener('click', controlMode)
-
-    return () => {
-      slider.removeEventListener('click', controlMode)
+    if (localStorage.theme === 'dark') {
+      setisDarkMode(true)
+      document.querySelector('html').classList.add('dark')
     }
+
   }, [isDarkMode])
 
-  return <div className={`dark-mode-slider flex rounded-full ${isDarkMode ? 'bg-gray-900 flex-row-reverse' : 'bg-white'} w-24 p-1 items-center gap-2 cursor-pointer`}>
-    <div className={`button w-8 h-8 ${isDarkMode ? 'bg-white flex-row-reverse' : 'bg-gray-300'} rounded-full`}></div>
-    <div className={`status capitalize ${isDarkMode ? 'text-white' : ''}`}>{isDarkMode ? 'Dark' : 'Light'}</div>
+  return <div onClick={switchTheme} className={`dark-mode-slider flex rounded-full ${isDarkMode ? 'bg-gray-800 flex-row-reverse' : 'bg-white'} w-24 p-1 items-center gap-2 cursor-pointer`}>
+    <div className={`button w-8 h-8 ${isDarkMode ? 'bg-gray-500 flex-row-reverse' : 'bg-gray-300'} rounded-full`}></div>
+    <div className={`status capitalize ${isDarkMode ? 'text-white' : ''}`}>{isDarkMode ? 'Dark' : 'Light'}
+    </div>
   </div>
 }
 
@@ -61,12 +62,10 @@ export default function Navbar() {
   })
 
   return (
-    <div className={`navbar fixed left-0 right-0 z-10 bg-gray-200 min-h-0 py-4 px-8 transition duration-100 ease-in-out transform ${isScrolledDown ? '-translate-y-20' : ''}`}>
+    <div className={`navbar bg-gray-200 dark:bg-gray-900 fixed left-0 right-0 z-10  min-h-0 py-4 px-8 transition duration-500 ease-in-out transform ${isScrolledDown ? '-translate-y-20' : ''}`}>
       <div className="container mx-auto flex items-center justify-between">
         <Brand />
-        <div className="navigation">
-          <DarkModeSlider />
-        </div>
+        <DarkModeSlider />
       </div>
     </div>
   )
