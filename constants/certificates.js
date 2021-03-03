3cfb3aecedd82e049f1d50518bb886fe96657bc8
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-03-03 08:24:14 +0700

    change data structure + non-activate a jquery cert

--- constants/certificates.js
+++ constants/certificates.js
@@ -1,32 +1,42 @@
 export default [
   {
-    name: 'Python Fundamental Programming - Dicoding',
+    name: 'Python Fundamental Programming',
+    issuer: 'Dicoding',
+    issuerURL: 'https://dicoding.com',
     imageFileName: 'Python Fundamental Programming - Dicoding-min.jpg',
     credentialLink: 'https://www.dicoding.com/certificates/ERZR50VMOPYV',
   },
   {
-    name: 'SOLID Principle - Dicoding',
+    name: 'SOLID Principle',
+    issuer: 'Dicoding',
+    issuerURL: 'https://dicoding.com',
     imageFileName: 'SOLID Principle - Dicoding-min.jpg',
     credentialLink: 'https://www.dicoding.com/certificates/72ZD2QQ3QZYW',
   },
   {
+    name: 'PHP Tutorial Course',
+    issuer: 'Sololearn',
+    issuerURL: 'https://sololearn.com',
+    imageFileName: 'PHP Tutorial Course-min.jpg',
+    credentialLink: 'https://www.sololearn.com/Certificate/1059-6168276/pdf/',
+  },
+  {
     name: 'CSS Fundamental Course',
+    issuer: 'Sololearn',
+    issuerURL: 'https://sololearn.com',
     imageFileName: 'CSS Fundamental Course-min.jpg',
     credentialLink: 'https://www.sololearn.com/Certificate/1023-6168276/pdf/',
   },
   {
     name: 'HTML Fundamental Course',
+    issuer: 'Sololearn',
+    issuerURL: 'https://sololearn.com',
     imageFileName: 'HTML Fundamental Course-min.jpg',
     credentialLink: 'https://www.sololearn.com/Certificate/1014-6168276/pdf/',
   },
-  {
-    name: 'jQuery Tutorial Course',
-    imageFileName: 'jQuery Tutorial Course-min.jpg',
-    credentialLink: 'https://www.sololearn.com/Certificate/1082-6168276/pdf/',
-  },
-  {
-    name: 'PHP Tutorial Course',
-    imageFileName: 'PHP Tutorial Course-min.jpg',
-    credentialLink: 'https://www.sololearn.com/Certificate/1059-6168276/pdf/',
-  },
+  // {
+  //   name: 'jQuery Tutorial Course',
+  //   imageFileName: 'jQuery Tutorial Course-min.jpg',
+  //   credentialLink: 'https://www.sololearn.com/Certificate/1082-6168276/pdf/',
+  // },
 ]
