aec311b7100a4f1720ad9857159c049dd91c4655
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-03-03 08:29:38 +0700

    refresh look cert card + code refactoring

--- components/home/certificates.jsx
+++ components/home/certificates.jsx
@@ -1,13 +1,26 @@
 import certificates from '../../constants/certificates'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-const CertificateCard = (props) =>
-  <div className="certificate-card flex flex-col gap-4 p-4 bg-white dark:bg-gray-800 dark:text-gray-300 shadow-lg rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
+const CertificateCard = (props) => {
+  const {
+    imageFileName,
+    name,
+    issuerURL,
+    issuer,
+    credentialLink, } = props.certificateData
 
-    <img src={`/images/certificates/${props.certificateData.imageFileName}`} alt="Tel-U physics lab web project" className="rounded-xl" />
+  return (
+    <div className="certificate-card flex flex-col gap-4 p-4 bg-white dark:bg-gray-800 dark:text-gray-300 shadow-lg rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105">
 
-    <h3 className="certificate-title flex-grow font-semibold text-xl md:text-2xl">{props.certificateData.name}</h3>
-    <a href={props.certificateData.credentialLink} target="_blank" rel="noopener noreferrer" className="self-end px-8 py-2 rounded-md transition duration-500 ease-in-out bg-blue-200 hover:bg-blue-400 text-blue-800 font-bold">View credential</a>
-  </div>
+      <img src={`/images/certificates/${imageFileName}`} alt="Tel-U physics lab web project" className="rounded-xl" />
+
+      <h3 className="certificate-title font-bold text-xl md:text-2xl">{name}</h3>
+      <a href={issuerURL} target="_blank" rel="noopener noreferrer" className="certificate-issuer self-start flex-grow md:text-xl">{issuer}</a>
+      <a href={credentialLink} target="_blank" rel="noopener noreferrer" className="flex gap-4 items-center self-end px-6 py-2 rounded-md transition duration-500 ease-in-out border border-blue-600 hover:bg-blue-100 dark:hover:bg-blue-600 hover:border-opacity-0 text-blue-600 dark:text-blue-600 dark:hover:text-white font-bold">View credential
+    <FontAwesomeIcon icon={['fas', 'external-link-alt']} className="w-4" />
+      </a>
+    </div>)
+}
 
 export default function Certificates() {
 
