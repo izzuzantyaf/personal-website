d45def51703feb04efa73484fad6a92d6fa45e3b
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-04-06 12:12:39 +0700

    fix project image hover effect

--- components/home/projects.jsx
+++ components/home/projects.jsx
@@ -14,14 +14,12 @@ export default function Projects() {
             </div>
             <a href="https://labfisdas-telu.com" target="_blank" rel="noopener noreferrer" className="visit-me-btn self-center sm:self-start px-8 py-2 rounded-md transition duration-500 ease-in-out border border-blue-500 hover:bg-blue-500 hover:border-none hover:text-white  text-blue-500 font-bold">Visit me</a>
           </div>
-          <div className="project-image">
+          <div className="project-image shadow-xl rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105 overflow-hidden">
             <Image
               src="/images/projects/labfisdas.png"
               alt="Tel-U physics lab web project"
               width={1348}
               height={764}
-              // layout="intrinsic"
-              className="shadow-xl rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105"
             />
             {/* <img src="/images/projects/labfisdas.png" alt="Tel-U physics lab web project" className="shadow-xl rounded-xl transition duration-500 ease-in-out transform hover:-translate-y-1 hover:scale-105" /> */}
           </div>
