151b3a78cee4377a90a94a71aaef205dd897f1e0
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-04-06 22:30:01 +0700

    some config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,23 @@
 module.exports = {
   darkMode: 'class',
   future: {},
-  purge: ['./pages/**/*.jsx', './components/**/*.jsx'],
+  purge: {
+    content: [
+      './pages/**/*.{js,ts,jsx,tsx}',
+      './components/**/*.{js,ts,jsx,tsx}'
+    ],
+    options: {
+      safelist: [
+        'text-red-600',
+        'text-gray-600',
+        'text-blue-600',
+        'text-pink-600',
+        'text-blue-400',
+      ],
+      keyframes: true,
+      fontFace: true,
+    }
+  },
   theme: {
     fontFamily: {
       'sans': '"Nunito Sans", system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"'
