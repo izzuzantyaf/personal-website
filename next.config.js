c0900944d6bbefb06188ba3a7c3b5c002fdbfead
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-06-23 14:46:18 +0700

    hello PWA

---
+++ next.config.js
@@ -0,0 +1,10 @@
+// next.config.js
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    runtimeCaching
+  },
+});
--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,22 @@ import '../styles/global.css'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import Head from 'next/head'
 
 library.add(fab, fas)
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
