d781adbfcc54880c074aacb2e097dea9df37d5b6
Izzu Zantya Fawwas <izzuzantyaf@gmail.com>
2021-06-27 08:45:08 +0700

    install hotjar

--- pages/_app.js
+++ pages/_app.js
@@ -11,7 +11,18 @@ function MyApp({ Component, pageProps }) {
     <div>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
+      <!-- Hotjar Tracking Code for https://izzuzantyaf.space -->
+<script>
+    (function(h,o,t,j,a,r){
+        h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+        h._hjSettings={hjid:2474554,hjsv:6};
+        a=o.getElementsByTagName('head')[0];
+        r=o.createElement('script');r.async=1;
+        r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+        a.appendChild(r);
+    })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+</script>
+</Head>
       <Component {...pageProps} />
     </div>
   )
